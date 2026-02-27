f605d8baa7d99b182d62c7257e0921c46ff506c7
Debosmita-code <debosmita.code@gmail.com>
2026-02-27 22:23:32 +0530

    Initial DSA upload

---
+++ Arrays/Largest.java
@@ -0,0 +1,15 @@
+package Arrays;
+class Largest{
+    public static int largest(int[] arr) {
+        int max= arr[0];
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>max)
+                max=arr[i];
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 15, 30};
+        System.out.println("Largest element: " + largest(arr));
+    }
+}
